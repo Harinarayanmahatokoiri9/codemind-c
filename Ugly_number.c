73263342884f31ffe3ffead058204b8bfff264c5
Hari Narayan Mahato Koiri <121252915+Harinarayanmahatokoiri9@users.noreply.github.com>
2023-05-24 10:44:48 +0530

    #include<stdio.h> int main() {     int n,c=0;     scanf("%d",&n);     while(n!=1)     {         if(n%2==0)         {             n/=2;         }         else if(n%3==0)         {             n/=3;         }         else if(n%5==0)         {             n/=5;         }         else         {             printf("Not Ugly Number");             c=1;             break;         }     }     if(c==0)     {         printf("Ugly Number");     }     /*else     {         printf("Not Ugly Number");     }*/ }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,39 +1,35 @@
-# include <stdio.h>
-# include <string.h>
+#include<stdio.h>
 int main()
 {
-    int n,x=0;
+    int n,c=0;
     scanf("%d",&n);
-    if(n>0)
+    while(n!=1)
     {
-       while(n!=1) 
-       {  
-            if(n%5==0) 
-            {  
-                n/=5;  
-            } 
-            else if (n%3==0) 
-            {  
-                n /= 3;  
-            } 
-            else if (n % 2 == 0) 
-            {  
-                n /= 2;  
-            } 
-            else 
-            {  
-                printf("Not Ugly Number"); 
-                x = 1;  
-                break;  
-            }  
-        } 
-		if (x==0)
-		 { 
-            printf("Ugly Number");
+        if(n%2==0)
+        {
+            n/=2;
+        }
+        else if(n%3==0)
+        {
+            n/=3;
+        }
+        else if(n%5==0)
+        {
+            n/=5;
+        }
+        else
+        {
+            printf("Not Ugly Number");
+            c=1;
+            break;
         }
     }
-    else
+    if(c==0)
+    {
+        printf("Ugly Number");
+    }
+    /*else
     {
         printf("Not Ugly Number");
-    }
+    }*/
 }
